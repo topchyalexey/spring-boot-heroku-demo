88970056f5fecc2bc9587079d3df1d69511ab62c
Topchy Alexey <topchyalexey@gmail.com>
2016-09-01 09:10:04 +0300

    Init

--- src/main/java/com/company/qrdocs/Record.java
+++ src/main/java/com/company/qrdocs/Record.java
@@ -1,4 +1,4 @@
-package com.heroku.demo;
+package com.company.qrdocs;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
--- src/main/java/com/company/qrdocs/RecordRepository.java
+++ src/main/java/com/company/qrdocs/RecordRepository.java
@@ -1,4 +1,4 @@
-package com.heroku.demo;
+package com.company.qrdocs;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
